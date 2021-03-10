8fd06b18ff9a52c388a5534904093981b650a4de
Kapil Bulsara <dev@kapilbulsara.com>
2021-03-10 11:47:26 -0500

    delete folders and database script

--- create_test_env.php
+++ create_test_env.php
@@ -1,45 +1,30 @@
 <?php 
+/* @TODO works working
+- working
+2.0.0.0,2.0.0.0b1 - 2.1.0.2,  3.0.0.0 -  3.0.2.0
+- 2.2.0.0 - working with some php errors in template - seems like an opencart issue 
 
+
+
+- not working 
+all 1.x 
+-  2.2.0.0_a1 - b1 
+
+ */
 //   php cli_install.php install --db_hostname localhost \
 //                               --db_username root \
 //                               --db_password pass \
-//                               --db_database opencart \
+//                               --db_prefix demo_opencart \
 //                               --db_driver mysqli \
 //								 --db_port 3306 \
 //                               --username admin \
 //                               --password admin \
 //                               --email youremail@example.com \
+//                               --http_server http://localhost/opencart/
 
-$options = array(
-		'--db_hostname' => 'localhost',
-		'--db_username' => 'root',
-		'--db_password' => 'pass',
-		'--db_database' => 'opencart',
-		'--db_driver' => 'mysqli',
-		'--db_port' => '3306',
-		'--username' => 'admin',
-		'--password' => 'admin',
-		'--email' => 'youremail@example.com',
-		'--http_server' => 'http://localhost/opencart/'
+// php create_test_env.php  --db_hostname localhost  --db_username demo --db_password demo --db_prefix demo_opencart_  --username admin --password admin --email yourmail@example.com --http_server http://demo.localhost/opencart/
 
-);
-
-$options = getOptions($argv);
-
-$alternateOptions = array(
-		'--db_hostname'	=>'--db_host',
-		'--db_username' 	=>'--db_user',
-		'--db_password' 	=>'--db_password',
-		'--db_database' 	=>'--db_name',
-		'--db_driver' 		=>'',
-		'--db_port' 		=>'',
-		'--username' 		=>'--username',
-		'--password' 		=>'--password',
-		'--email' 			=>'--email',
-		'--http_server' 	=>'--http_server'
-);
-
-print_r($options);
+//print_r($options);
 // BEGIN clone repos
 $repoName = 'opencart_master';
 //exec("git clone https://github.com/opencart/opencart.git $repoName");
@@ -48,19 +33,18 @@ exec("git ls-remote --tags $repoName", $tags);
 foreach( $tags as  $line){
 
 		$tag = substr($line, strpos($line, 'refs/tags/') + 10 ); 
-		$tag = str_replace('.', '', $tag);
-		//exec("git clone $repoName --branch=$tag $tag");
-		//exec("cd $tag && git checkout -b master");
-		// END clone repos
+		exec("git clone $repoName --branch=$tag $tag");
+		exec("cd $tag && git checkout -b master");
+// END clone repos
 		$options  = $argv; 
 
 		$hostname =  $options[array_search('--db_hostname', $options) +1 ];
 		$dbuser  = $options[array_search('--db_username', $options) +1 ];
 		$dbpass = $options[array_search('--db_password', $options) +1 ];
-		$database = $options[array_search('--db_prefix', $options) +1 ] . $tag;
+		$database = $options[array_search('--db_prefix', $options) +1 ] . str_replace('.', '', $tag);	
 		createDB($hostname, $dbuser ,  $dbpass, $database );
 
-		$options[array_search('--http_server', $options) + 1] = $options[array_search('--http_server', $options) + 1] . $tag;
+		$options[array_search('--http_server', $options) + 1] = $options[array_search('--http_server', $options) + 1] .   "$tag/upload/";
 
 		if(substr($tag, 0,1) == '1'){
 				//alternate option names
@@ -68,13 +52,18 @@ foreach( $tags as  $line){
 				$options[array_search('--db_username', $options)] = '--db_user';
 				$options[array_search('--db_prefix', $options)] = '--db_name';
 				$options[array_search('--db_name', $options) + 1] = $database;
+				$options['--agree_tnc'] = 'yes';
 
 		}else{
 
 				$options[array_search('--db_prefix', $options)] = '--db_database';
 				$options[array_search('--db_database', $options) + 1] = $database;
 		}
-		exec("cd $tag/upload/install/ && php cli_install.php " . implode(" ", $argv) . " --db_database demo_opencart_$tag  --http_server " .  $options['--db_password'] . "$tag" );
+
+		array_splice($options, 0,1);
+
+		exec("cd $tag/upload/install/ && php cli_install.php install " . implode(" ", $options) );
+	//	print_r($options);
 
 }
 
